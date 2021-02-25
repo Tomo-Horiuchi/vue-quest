6b31439999b64a517673c02a651c268e16777a0c
Tomo-Horiuchi <horiuchi.github@gmail.com>
2021-02-26 03:21:17 +0900

    VueQuest #2

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,35 +1,31 @@
 <template>
-    <v-container>
-        <v-card
-            class="my-10"
-            elevation="10"
-            rounded="xl"
-            width="100%"
-        >
-        <v-container
-            class="mx-10 text-center"
-        >
-        <v-form>
-            <v-text-field
-                label="YouTube動画ID"
-                        v-model="movieUrl"
-                        placeholder="動画URLの【v= 】の後に続く英数字"
-                        style="width: 300px"
-                    />
-                        {{ movieUrl }}
-                </v-form>
-            </v-container>
-        </v-card>
-    </v-container>
+  <v-container>
+    <input-form
+      formRounded="xl"
+      :formElevation="formElevation"
+      @storeMovie="storeMovie"
+    />
+  </v-container>
 </template>
 
 <script>
-    export default {
-        name: 'Top',
-        data () {
-            return {
-                movieUrl: "",
-            }
-    }
-    }
+  import InputForm from './InputForm.vue'
+
+  export default {
+    components: {
+      InputForm,
+    },
+
+    data () {
+      return {
+        formElevation: "10",
+      }
+    },
+
+    methods: {
+      storeMovie (movieUrl, comment) {
+        console.log(movieUrl, comment)
+      },
+    },
+  }
 </script>
